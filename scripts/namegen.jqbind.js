46a28cea338cb849520e7dd9fc49744ad619214c
Karel Wintersky <karel.wintersky@yandex.ru>
2017-07-30 00:22:07 +0300

    * fixes

--- scripts/namegen.jqbind.js
+++ scripts/namegen.jqbind.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     // function must be declared inside $.ready()
     var initDictionary = function(state) {
         $("#actor-generate-name").prop('disabled', !!(state.dict == ''));
